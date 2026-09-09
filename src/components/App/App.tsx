6da0e6019d30b76b3fa91d3a5abd2d5e54846991
Kostiantyn Shevchuk <constantine.shevchuk@gmail.com>
2026-09-10 00:49:20 +0300

    Step_1_2_3

---
+++ src/components/App/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import CafeInfo from '../CafeInfo/CafeInfo';
+import type { Votes, VoteType } from '../../types/votes';
+import css from './App.module.css';
+
+function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (type: VoteType) => {
+    setVotes({
+      ...votes,
+      [type]: votes[type] + 1,
+    });
+  };
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+    </div>
+  );
+}
+
+export default App;
